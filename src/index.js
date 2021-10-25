745a22e0c53c4c8c0b2bd38922ad11822b047413
Thomas Lyver <tjlyver@gmail.com>
2021-10-25 15:38:19 -0400

    Sass file
    Imports bootstrap.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import 'bootstrap/scss/bootstrap.scss';
-// import '../node_modules/bootstrap/dist/scss/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
