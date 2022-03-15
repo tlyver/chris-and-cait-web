60e2414fa14bbe0463d12cbc00fc7300df7654c1
Thomas Lyver <tlyver@williamhill.us>
2022-03-15 19:18:05 -0400

    Accommodations Component
    Adds an accommodations component.
    Passes in props.

---
+++ src/accommodation.js
@@ -0,0 +1,95 @@
+import './accommodation.scss';
+
+
+const Event = props => {
+  const clickMap = () => {
+    console.log("hello");
+  };
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener, noreferrer')
+    if (newWindow) newWindow.opener = null
+  };
+
+  const onClickUrl = (url) => {
+    return () => openInNewTab(url)
+  }
+
+  return (
+    <>
+      <div className="row mt-4 mb-5">
+      {
+        //<div className="row row-cols-1 row-cols-md-2 mt-4 mb-5">
+      }
+        <div className="col d-none d-md-block col-md-1 col-lg-3"></div>
+        <div className="col d-flex flex-direction--column align-items-center justify-content-center pt-3 pb-3">
+        {
+          //<div className="col d-flex flex-direction--column align-items-center justify-content-center border-right-responsive pt-3 pb-3">
+        }
+          <h2 className="underground">{props.name}</h2>
+          <div className="d-flex flex-column address">
+            <div>
+              {props.street}
+            </div>
+            <div>
+              {props.city}
+            </div>
+          </div>
+          <a
+            className="phone-number underground is-20 pt-3 pb-3"
+            href="tel:PHONE_NUM"
+          >
+            {props.phone}
+          </a>
+          <div className="is-20">{props.description}</div>
+          <a
+            href="https://www.caesars.com/caesars-palace"
+            onClick={onClickUrl('https://www.caesars.com/caesars-palace')}
+            className="btn btn-sm btn-outline-dark mt-5 underground"
+            role="button"
+            aria-pressed="true"
+            target="_blank"
+          >
+            View
+          </a>
+        </div>
+        {
+        // <div className="col d-flex flex-column align-items-center justify-content-center underground pt-3 pb-3">
+        //   <div>{props.location}</div>
+        //   <a
+        //     href="https://goo.gl/maps/Kh7XiTXbGqpjZutH9"
+        //     onClick={onClickUrl('https://goo.gl/maps/Kh7XiTXbGqpjZutH9')}
+        //     className="btn btn-sm btn-outline-dark mt-3 underground"
+        //     role="button"
+        //     aria-pressed="true"
+        //     target="_blank"
+        //   >
+        //     Map
+        //   </a>
+        //   {
+        //     /*
+        //     <a
+        //     onClick={onClickUrl('https://stackoverflow.com')}
+        //     className="btn btn-sm btn-outline-dark underground mt-2"
+        //     role="button"
+        //     aria-pressed="true"
+        //     >
+        //     Add To Calendar
+        //     </a>
+        //     */
+        //   }
+        // </div>
+        }
+        <div className="col d-none d-md-block col-md-1 col-lg-3"></div>
+      </div>
+
+
+      <div className="row py-4 justify-content-center d-block">
+        <div className="col-2 border-bottom border-dark m-auto">
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default Event;
--- src/routes/accommodations.js
+++ src/routes/accommodations.js
@@ -1,12 +1,31 @@
+import Accommodation from '../accommodation';
+
 export default function Accommodations() {
   return (
-    <div className="row mt-4 justify-content-center">
-      <div className="col pb-4 col-lg-6 col-md-8 col-10 border-bottom border-dark">
-        <h2 className="section-header">Accommodations</h2>
-        <div className="eaves">
-          Stay With Us!
+    <>
+      <div className="row mt-4 justify-content-center">
+        <div className="col pb-4 col-lg-6 col-md-8 col-10 border-bottom border-dark">
+          <h2 className="section-header">Accommodations</h2>
+          <div className="eaves">
+            Stay with us!
+          </div>
         </div>
       </div>
-    </div>
+
+      <Accommodation
+        name="Hotel Name"
+        street="2187 Bruynswick Rd,"
+        city="Walkill, NY 12589"
+        phone="(914) 826 7185"
+        description="The Waterfront is located in the heart of historic Salem. It's in walking distance from Pickering Wharf, and Salem Harbor. Shuttle services will be provided from the Salem Waterfront to and from the ceremony and reception. We have a room block secured. Please use Jancsy- Figueredo Room Block when booking!"
+      />
+      <Accommodation
+        name="Other Hotel"
+        street="2187 Bruynswick Rd,"
+        city="Walkill, NY 12589"
+        phone="(914) 826 7720"
+        description="A newly built hotel, with a great rooftop bar for cocktails and small bites. The Hotel Salem is a 0.5 mile walk from the Salem Waterfront Hotel, if you choose to utilize the shuttle service."
+      />
+    </>
   );
 }
