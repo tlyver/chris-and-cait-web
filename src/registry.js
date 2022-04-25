3f67fcf4d5f2f0f2cd1c5ce0d6dd001541c41078
Thomas Lyver <tlyver@williamhill.us>
2022-04-25 08:34:12 -0400

    Adds Registry links and description.

---
+++ src/registry.js
@@ -0,0 +1,52 @@
+import './accommodation.scss';
+
+
+const Event = props => {
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener, noreferrer')
+    if (newWindow) newWindow.opener = null
+  };
+
+  const onClickUrl = (url) => {
+    return () => openInNewTab(url)
+  }
+
+  return (
+    <>
+      <div className="row mt-4 mb-5">
+      {
+        //<div className="row row-cols-1 row-cols-md-2 mt-4 mb-5">
+      }
+        <div className="col d-none d-md-block col-md-1 col-lg-3"></div>
+        <div className="col d-flex flex-direction--column align-items-center justify-content-center pt-3 pb-3">
+        {
+          //<div className="col d-flex flex-direction--column align-items-center justify-content-center border-right-responsive pt-3 pb-3">
+        }
+          <h2 className="underground">{props.name}</h2>
+
+          <div className="is-18">{props.description}</div>
+          <a
+            // href={props.link}
+            onClick={onClickUrl(props.buttonLink)}
+            className="btn btn-sm btn-outline-dark mt-5 underground"
+            role="button"
+            aria-pressed="true"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {props.buttonLabel}
+          </a>
+        </div>
+        <div className="col d-none d-md-block col-md-1 col-lg-3"></div>
+      </div>
+
+
+      <div className="row py-4 justify-content-center d-block">
+        <div className="col-2 border-bottom border-dark m-auto">
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default Event;
--- src/routes/registry.js
+++ src/routes/registry.js
@@ -1,12 +1,29 @@
-export default function Registry() {
+import Registry from '../registry';
+
+export default function Registrys() {
   return (
-    <div className="row mt-4 justify-content-center">
-      <div className="col pb-4 col-lg-6 col-md-8 col-10 border-bottom border-dark">
-        <h2 className="section-header">Registry</h2>
-        <div className="eaves">
-          Buy Us A House!
+    <>
+      <div className="row mt-4 justify-content-center">
+        <div className="col pb-4 col-lg-6 col-md-8 col-10 border-bottom border-dark">
+          <h2 className="section-header">Registry</h2>
+          <div className="eaves is-20">
+            Please do not feel obligated to buy us a gift - your presence is a present in itself! However, should you choose to buy one, we’ve added our hearts’ desires to this registry. We can’t wait to celebrate with you on our wedding day!
+          </div>
         </div>
       </div>
-    </div>
+
+      <Registry
+        // description="We can have a description for Zola(??)"
+        buttonLabel="Zola"
+        buttonLink="https://www.zola.com/registry/chrisandcaitwedding"
+        name="Zola"
+      />
+      <Registry
+        // description="We can have a description for Amazon(??)"
+        buttonLabel="CB2"
+        buttonLink="https://www.cb2.com/gift-registry/registrant-list/6409329"
+        name="CB2"
+      />
+    </>
   );
 }
